d36b0dfa44bdbfa7cb2e8314544f12cb8dbd0958
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-22 15:41:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,31 +1,9 @@
 def new_hash
-  hash = Hash.new
-  hash
+  {}
 end
 
 def my_hash
-  h = {:new_hash => 3}
-  h
-end
-
-def pioneer
-  h = { :name => "Grace Hopper"}
-  
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  h
-end
-
-def id_generator
-  h = { :id => 3 }
-  
-  h
-  # return a hash with a key :id assigned to positive integer
-end
-
-def my_hash_creator(key, value)
-  h = {:name => "Grace Hopper"}
-  h
-  # return a hash that includes the key and value parameters passed into this method
+  { "Programming" => "is great!" }
 end
 
 def pioneer
